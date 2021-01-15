f09160881e670e40af38adb11cccf35515553fba
Sergio <sergioburca@gmail.com>
2021-01-15 18:20:34 -0500

    file directory modified

--- src/ActionButton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-const ActionButton = ({execute,actionName, status}) =>{
-    return (
-        <button onClick={execute}>{actionName}{status && status}</button>
-    )
-}
-export default ActionButton
