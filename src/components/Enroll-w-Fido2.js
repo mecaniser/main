f15bc2f39b2fe3d922b0c42319f27f6e7086c102
Sergio <sergioburca@gmail.com>
2021-01-21 22:09:19 -0500

    using default xmui handler

--- src/components/Enroll-w-Fido2.js
+++ src/components/Enroll-w-Fido2.js
@@ -2,17 +2,13 @@ import React, { useEffect, useState } from "react";
 import "../App.css";
 import ActionButton from "./ActionButton";
 var sdk = new xmsdk.XmSdk();
+var UiHandler = new xmui.XmUIHandler()
 
 var journeyId = ["register_fido", "deregister_fido", "authenticator_status"];
 var paramsInput =
   '{"wfSessionId":"3e773ac1-cb1c-484f-a31b-ff9e6e13f0dd","manifestJWT":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI4MDJiZDc3Ny01YzhkLTQ3ZjgtYWRkMC1jMWM1MDhkODQ5MTEiLCJpc3MiOiJPMyIsImV4cCI6MTYxMTc4MTcwMywiaWF0IjoxNjExMTc2OTAzLCJqdGkiOiIzZTc3M2FjMS1jYjFjLTQ4NGYtYTMxYi1mZjllNmUxM2YwZGQifQ.rq8EyRe0CuY_LeZcbc1S4QNg5sKlMf0HlNCgrg4-v60"}';
 var params = paramsInput ? JSON.parse(paramsInput) : paramsInput;
 var username = "Sergio2";
-// var username = "Sergio";
-var transmitContainer = document.getElementById("transmitContainer");
-var clientContext = {
-  uiContainer: transmitContainer,
-};
 
 const EnrollWithFido2 = () => {
   const [msg, setmsg] = useState("");
@@ -28,24 +24,6 @@ const EnrollWithFido2 = () => {
     sdk.setConnectionSettings(settings);
     sdk.setUiAssetsDownloadMode(com.ts.mobile.sdk.UIAssetsDownloadMode.Disable);
 
-    // const uiHandler = new xmui.XmUIHandler();
-    function CustomUIHandler() {
-      xmui.XmUIHandler.call(this);
-    }
-
-    CustomUIHandler.prototype = Object.create(xmui.XmUIHandler.prototype);
-    CustomUIHandler.prototype.constructor = CustomUIHandler;
-
-    // CustomUIHandler.prototype.processJsonData = function (jsonData) {
-    //   return Promise.resolve(
-    //     com.ts.mobile.sdk.JsonDataProcessingResult.create(true)
-    //   );
-    // };
-
-    CustomUIHandler.prototype.handlePolicyRejection = function () {
-      return Promise.resolve(com.ts.mobile.sdk.ConfirmationInput.create(-1));
-    };
-
     sdk
       .initialize()
       .then((results) => {
@@ -56,74 +34,17 @@ const EnrollWithFido2 = () => {
         console.error(`Transmit SDK initialization error!: ${error._message}`);
         setmsg(`Sdk initialized with error:  ${error._message}`);
       });
-    var myUIHandler = new CustomUIHandler();
-    sdk.setUiHandler(myUIHandler);
+    
+    sdk.setUiHandler(UiHandler);
+    
   }, []);
-  // const enrollmentStatusHandler = () => {
-  //   sdk
-  //     .authenticate(username, journeyId[2], params, {})
-  //     .then((authStatus) => {
-  //       console.log(
-  //         `Enrollment Status: ${authStatus._deviceId} and username ${username}`
-  //       );
-  //     })
-  //     .then(() => {
-  //       sdk.logout().then((logOutResponse) => {
-  //         console.log(`Session deactivated on Enrollment Status: ${logOutResponse}`);
-  //         setmsg(`Enrollment Status logout ${logOutResponse}`)
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       console.log(`Enrollment Status: ${error._message}`);
-  //     })
-  //     .catch((error) => console.log(`Enrollment Status error: ${error._message}`));
-  // };
-
-  // const registerHandler = () => {
-  //   sdk
-  //     .authenticate(username, journeyId[0], params, {})
-  //     .then((authStatus) => {
-  //       console.log(
-  //         `Registered with: ${authStatus._deviceId} and username ${username}`
-  //       );
-  //     })
-  //     .then(() => {
-  //       sdk.logout().then((logOutResponse) => {
-  //         console.log(`Session deactivated on Authenticate and Enrollment: ${logOutResponse}`);
-  //         setmsg(`Enrolled as ${username}`)
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       console.log(`Authenticate error: ${error._message}`);
-  //     })
-  //     .catch((error) => console.log(`Authentication error: ${error._message}`));
-  // };
-
-  // const deregisterHandler = () => {
-  //   sdk
-  //     .authenticate(username, journeyId[1], params, {})
-  //     .then((authStatus) => {
-  //       console.log(`Unregistered ${username}`);
-  //       setmsg(`${username} was unregistered`)
-  //     })
-  //     .then(() => {
-  //       sdk.logout().then((logOutResponse) => {
-  //         console.log(`Session deactivated on Unregister: ${logOutResponse}`);
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       console.log(`De-Registration error: ${error._message}`);
-  //     })
-  //     .catch((error) => {
-  //       console.log(`De-Registration authenticate error: ${error._message}`);
-  //     });
-  // };
+ 
   const fidoJourney = (journey) => {
     sdk
       .authenticate(username, journey, params, {})
       .then((authStatus) => {
         console.log(
-          `Registered with: ${authStatus._deviceId} and username ${username}`
+          `${journey} with: ${authStatus._deviceId} and username ${username}`
         );
       })
       .then(() => {
