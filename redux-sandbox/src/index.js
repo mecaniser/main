a192113c148ad07ce447ae630425c17f30852c8c
mecaniser <sergioburca@gmail.com>
2021-04-12 22:48:56 -0400

    last changes

--- redux-sandbox/src/index.js
+++ redux-sandbox/src/index.js
@@ -1,15 +1,24 @@
-const reducer = (state = 0, action) => {
-  switch (action.type) {
-    case "INC":
-      return state + 1;
-    default:
-      return state;
-  }
+import { createStore, bindActionCreators } from "redux";
+import reducer from "./reducer";
+import * as actions from "./actions";
+
+const store = createStore(reducer);
+const { dispatch } = store;
+
+const { inc, dec, rnd } =  
+  bindActionCreators(actions, dispatch);
+
+document.getElementById("inc").addEventListener("click", inc)
+
+document.getElementById("dec").addEventListener("click", dec)
+
+document.getElementById("rnd").addEventListener("click", () => {
+  const payload = Math.floor(Math.random() * 10);
+  rnd(payload);
+});
+
+const update = () => {
+  document.getElementById("counter").innerHTML = store.getState();
 };
 
-let state = reducer(undefined, {});
-
-state = reducer(state, { type: "INC" });
-console.log(state);
-state = reducer(state, { type: "INC" });
-console.log(state);
+store.subscribe(update);
