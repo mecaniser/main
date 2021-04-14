5e9f6d312fdafbb8791e443e538da0455a750ccd
mecaniser <sergioburca@gmail.com>
2021-04-14 12:13:31 -0400

    css included; commercial file structure change

--- todo-react/src/components/app-header.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const AppHeader = () => <h1>My TODO list </h1>;
-export default AppHeader
--- todo-react/src/index.js
+++ todo-react/src/index.js
@@ -1,23 +1,7 @@
 import React from "react";
-import RD from "react-dom";
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
+import ReactDOM from "react-dom";
+import App from "./components/app"
 
-const App = () => {
-  const todoData = [
-    { label: "Drink Coffee", important: false, id:1},
-    { label: "Build app yourself", important: true, id:2 },
-    { label: "Drink Coffee again", important: false, id:3 },
-  ];
-  return (
-    <div>
-      <span>{new Date().toString()}</span>
-      <AppHeader />
-      <SearchPanel />
-      <TodoList todos={todoData} />
-    </div>
-  );
-};
+import "./index.css";
 
-RD.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
