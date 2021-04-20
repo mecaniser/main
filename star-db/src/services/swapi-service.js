7f54cf6777ed288d1e4df36c146125270b91f2bf
mecaniser <sergioburca@gmail.com>
2021-04-20 14:56:48 -0400

    adding empty components

---
+++ star-db/src/services/swapi-service.js
@@ -0,0 +1,33 @@
+export default class SwApiService {
+    _apiBase = "https://swapi.dev/api";
+    async getRsrc(url) {
+      const res = await fetch(`${this._apiBase}${url}`);
+  
+      if (!res.ok) {
+        throw new Error(`Could not fetch ${url} , received  ${res.status}`);
+      }
+      const body = await res.json();
+      return body;
+    }
+   async getAllPeople() {
+       const res = await this.getRsrc(`/people/`);
+      return res.results
+    }
+    getPerson(id) {
+      return this.getRsrc(`/people/${id}/`);
+    }
+   async getAllPlanets() {
+       const res = await this.getRsrc(`/planets/`);
+      return res.results
+    }
+    getPlanet(id) {
+      return this.getRsrc(`/planet/${id}/`);
+    }
+   async getAllStarShips() {
+       const res = await this.getRsrc(`/starships/`);
+      return res.results
+    }
+    getStarShip(id) {
+      return this.getRsrc(`/starship/${id}/`);
+    }
+  }
