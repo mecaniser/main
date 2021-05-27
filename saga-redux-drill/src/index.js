e653f4131c098a813d471f1b2ef3781c904c583e
mecaniser <sergioburca@gmail.com>
2021-05-27 13:58:13 -0400

    createStore-wrapping main component with store

--- saga-redux-drill/src/index.js
+++ saga-redux-drill/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 import axios from "axios";
 import reducers from "./reducers";
-import {Provider} from "react-redux";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+
+const store = createStore(reducers);
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api"
+axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    , document.getElementById('root')
+  </Provider>
 );
-
