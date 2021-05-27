59464afbeb7005d726189073f9f2c4ba1389cd90
mecaniser <sergioburca@gmail.com>
2021-05-27 13:23:48 -0400

    installing npm pkgs and adding axios

--- saga-redux-drill/src/index.js
+++ saga-redux-drill/src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api"
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
