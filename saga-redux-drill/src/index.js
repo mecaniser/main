359a2685d4e58efe5cb1be6356cc5975bfbf6a73
mecaniser <sergioburca@gmail.com>
2021-05-27 13:51:12 -0400

    importing reducers and Provider

--- saga-redux-drill/src/index.js
+++ saga-redux-drill/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import axios from "axios";
+import reducers from "./reducers";
+import {Provider} from "react-redux";
 
 axios.defaults.withCredentials = true;
 axios.defaults.baseURL = "http://rem-rest-api.herokuapp.com/api"
