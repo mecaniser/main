ba03569c0607b87d85a71fb4e257ddea2c7a0300
mecaniser <sergioburca@gmail.com>
2021-09-07 19:47:42 -0400

    add routes

---
+++ RESTful-Web-Services/routes/bookRouter.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const booksControllers = require("../controllers/booksControllers")
+
+const bookRouter = express.Router()
+const controller = require("../controllers/booksControllers")
+
+function routes(Book) {
+  const controller = booksControllers(Book)
+  bookRouter.route("/books")
+    .post(controller.post)
+    .get(controller.get)
+  bookRouter.use("/books/:bookId", (req, res, next) => {
+    const query = req.params.bookId
+
+    Book.findById(query, (err, book) => {
+      if (err) return res.send(err)
+      if (book) {
+        req.book = book;
+        return next()
+      }
+      return res.sendStatus(404)
+    })
+  })
+  bookRouter.route("/books/:bookId")
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title
+      book.author = req.body.title
+      book.genre = req.body.title
+      book.read = req.body.title
+      req.book.save((err) => {
+        err ? res.send(err) : res.json(book)
+      })
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) delete req.body._id
+      Object.entries(req.body).forEach(element => {
+        const key = element[0]
+        const value = element[1]
+        book[key] = value
+      });
+      req.book.save((err) => {
+        err ? res.send(err) : res.json(book)
+      })
+    })
+    .delete((req, res) => {
+          debugger
+          req.book.remove((err) => {
+            err ? res.send(err) : res.sendStatus(204)
+          })
+        })
+  return bookRouter;
+}
+
+module.exports = routes
