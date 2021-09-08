0eddcb944eede9e4a30111ca4bc1e584ea46bd21
mecaniser <sergioburca@gmail.com>
2021-09-08 09:47:31 -0400

    unit test fix

--- RESTful-Web-Services/controllers/booksControllers.js
+++ RESTful-Web-Services/controllers/booksControllers.js
@@ -1,6 +1,12 @@
 function booksControllers(Book) {
   function post(req, res) {
     const book = new Book(req.body)
+
+    if(!req.body.title) {
+      res.status(400)
+      return res.send("Title is required")
+    }
+    
     book.save()
     res.status(201)
     return res.json(book)
