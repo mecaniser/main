54c4ceb82871e51a577849b1cf1c659416d25f57
Sergio Burca <sergioburca@gmail.com>
2024-12-28 16:34:51 -0500

    add header

---
+++ ParkRight-Solutions/semi-truck-parking-app/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Right Parking Solutions</h1>
+    </header>
+  );
+};
+
+export default Header;
