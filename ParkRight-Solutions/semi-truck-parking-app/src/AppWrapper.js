88e3046924e95da9da280184f7deeebf304dd607
Sergio Burca <sergioburca@gmail.com>
2024-12-28 15:52:53 -0500

    add app wrapper

--- ParkRight-Solutions/semi-truck-parking-app/src/AppWrapper.js
+++ ParkRight-Solutions/semi-truck-parking-app/src/AppWrapper.js
@@ -1,23 +1,33 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import TopNavBar from './components/TopNavBar';
 // import Dashboard from './pages/Dashboard';
 // import BookingPage from './pages/BookingPage';
 
-function App() {
+const AppWrapper = () => {
+  const location = useLocation();
+
   return (
-    <Router>
-      <TopNavBar />
+    <>
+      {location.pathname !== '/' && <TopNavBar />}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         {/* <Route path="/booking" element={<BookingPage />} /> */}
       </Routes>
-    </Router>
+    </>
   );
-}
+};
+
+const App = () => (
+  <Router>
+    <AppWrapper />
+  </Router>
+);
 
 export default App;
