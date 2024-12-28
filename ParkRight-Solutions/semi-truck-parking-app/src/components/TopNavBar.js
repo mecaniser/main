772db0a32fda802beeedbd862efe9bc36d1b3eee
Sergio Burca <sergioburca@gmail.com>
2024-12-28 16:34:31 -0500

    relocate top nav css

--- ParkRight-Solutions/semi-truck-parking-app/src/components/TopNavBar.js
+++ ParkRight-Solutions/semi-truck-parking-app/src/components/TopNavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import './TopNavBar.css';
+import '../styles/TopNavBar.css';
 
 const TopNavBar = () => {
   const location = useLocation();
