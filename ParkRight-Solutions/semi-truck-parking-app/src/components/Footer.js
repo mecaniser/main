893f38a8d897b6f8825b1d91f80a08ee4622b2ca
Sergio Burca <sergioburca@gmail.com>
2024-12-29 11:33:00 -0500

    footer change contact info

--- ParkRight-Solutions/semi-truck-parking-app/src/components/Footer.js
+++ ParkRight-Solutions/semi-truck-parking-app/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; 2025 Right Solution Truck Parking. All rights reserved.</p>
-      <p>Contact us: info@rightparkingsolutions.com</p>
+      <p>Contact us: info@rstp.com</p>
     </footer>
   );
 };
