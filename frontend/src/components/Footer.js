5dcaceea80a277ba956acdd4cee8d51aa1dcd218
Sergio Burca <sergioburca@gmail.com>
2025-01-03 17:24:30 -0500

    feat: remove contact information from Footer component

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,6 @@ const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; 2025 Right Solution Truck Parking. All rights reserved.</p>
-      <p>Contact us: info@rstp.com</p>
     </footer>
   );
 };
